fe48bb6baf1745aa5a9593b4b198146764bec255
Nikita Tarutin <43347881+dogeatsdog@users.noreply.github.com>
2019-04-22 23:43:31 +0300

    a

--- game.js
+++ game.js
@@ -5,15 +5,28 @@ var ctx = cvs.getContext("2d");
 var bg = new Image();
 var asteroid = new Image();
 var shuttle = new Image();
-var fireball = new Image();
+let fireball = new Image();
+let exploded = new Image()
 
 bg.src = "img/background.jpg";
 asteroid.src = 'img/asteroid.jpg'
 shuttle.src = 'img/shuttle.png'
 fireball.src = 'img/fireball.png'
+exploded.src = 'img/fireball.png'
 
-document.addEventListener("contextmenu", moveDown);
-document.addEventListener("click", moveUp );
+
+document.addEventListener("keydown", move);
+
+function move(event) {
+  if (event.key === 'ArrowUp'){
+    event.preventDefault()
+    moveUp()
+  } else if (event.key === 'ArrowDown'){
+    event.preventDefault()
+    moveDown()
+  }
+}
+
 
 function moveUp(){
   yPos -= 20;
@@ -34,7 +47,7 @@ var yPos = 150;
 
 var shoot = [];
 shoot[0] = {
-  x: xPos ,
+  x: xPos,
   y: yPos+ 17
 }
 
@@ -42,15 +55,33 @@ shoot[0] = {
 function draw() {
  ctx.drawImage(bg, 0, 0);
 
+ for(var i=0; i<shoot.length; i++){
+   ctx.drawImage(fireball,shoot[i].x, shoot[i].y)
+   shoot[i].x++;
+
+   if (shoot[i].x == 120){
+    shoot.push({
+      x: xPos,
+      y: yPos + 17
+    });
+   }
+
+  
+
+  }
+
+
+
 for(var i=0; i<ast.length; i++){
   ctx.drawImage(asteroid,ast[i].x, ast[i].y)
   ast[i].x--;
-//var pos = Math.floor(Math.random()*cvs.width)
+var pos = Math.floor(Math.random()*cvs.width)
 if (ast[i].x == 400){
  ast.push({
    x: cvs.width,
    y: Math.floor(Math.random()*cvs.height)
  });
+ console.log(ast)
 }
 
 if (xPos + shuttle.width >= ast[i].x &&
@@ -59,20 +90,17 @@ if (xPos + shuttle.width >= ast[i].x &&
     yPos <= ast[i].y + asteroid.height) {
   location.reload()
 }
+for(var j=0; j<shoot.length;j++){
+  if (shoot[j].x + fireball.width - 8 >= ast[i].x &&
+     shoot[j].x  < ast[i].x + asteroid.height &&
+     shoot[j].y + fireball.height >= ast[i].y &&
+      shoot[j].y <= ast[i].y + asteroid.height) {
+      ast.splice(i,1) }
+
 }
 
-for(var i=0; i<shoot.length; i++){
-  ctx.drawImage(fireball,shoot[i].x, shoot[i].y)
-  shoot[i].x++;
+}
 
-  if (shoot[i].x == 120){
-   shoot.push({
-     x: xPos,
-     y: yPos + 17
-   });
-  }
-
- }
 
 
 ctx.drawImage(shuttle, xPos, yPos);
